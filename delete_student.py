d1a5774b02bf047c5c4f9a072bfaaf21082c2926
prime333152 <primepvpt@gmail.com>
2025-07-01 12:43:28 +0700

    delete_1-5 and teety

--- delete_student.py
+++ delete_student.py
@@ -6,7 +6,11 @@ cur = conn.cursor()
 # ลบนักเรียนที่มี ID = '37871'
 #('37617', 'ปัณณวิชญ์ จรูญภาค', '100'))
 cur.execute("DELETE FROM students WHERE id = ?", ('37575',))
-
+cur.execute("DELETE FROM students WHERE id = ?", ('37871',))
+cur.execute("DELETE FROM students WHERE id = ?", ('37577',))
+cur.execute("DELETE FROM students WHERE id = ?", ('37586',))
+cur.execute("DELETE FROM students WHERE id = ?", ('37615',))
+cur.execute("DELETE FROM students WHERE id = ?", ('37617',))
 conn.commit()
 conn.close()
 
